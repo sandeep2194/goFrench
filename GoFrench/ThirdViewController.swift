4a5ce0dab5b2f5c3e969f8cc6ee7f7d80069a713
Ling2022 <lling.he22@gmail.com>
2023-11-24 16:26:32 -0500

    fit the popup Items

--- GoFrench/ThirdViewController.swift
+++ GoFrench/ThirdViewController.swift
@@ -15,14 +15,105 @@ class ThirdViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setPopUpButtonType()
+        btnPopTense.isEnabled = false
+        //setPopUpButtonTense()
         // Do any additional setup after loading the view.
+       
     }
+    func setPopUpButtonType(){
+        let optionClosure = { [weak self] (action: UIAction) in
+                    guard let self = self else { return }
+                    print(action.title)
+
+                    // Update the menu for the second button based on the selection of the first button
+                    self.updatePopUpButtonTenseMenu(conjugationType: action.title)
+                }
+            
+        btnPopType.menu = UIMenu(children: [
+            UIAction(title: Segue.infinitive, state: .on, handler: optionClosure),
+            UIAction(title: Segue.participe, state: .on, handler: optionClosure),
+            UIAction(title: Segue.indicatif, state: .on, handler: optionClosure),
+            UIAction(title: Segue.subjonctif, state: .on, handler: optionClosure),
+            UIAction(title: Segue.conditionnel, state: .on, handler: optionClosure),
+            UIAction(title: Segue.imperatif, state: .on, handler: optionClosure),
+        ])
+    }
+    func setPopUpButtonTense() {
+            // Initial setup for the second button
+            btnPopTense.menu = UIMenu(children: [])
+    }
+    func updatePopUpButtonTenseMenu(conjugationType: String?) {
+        if(conjugationType==nil){
+            return;
+        }
+        // Clear the existing menu for the second button
+        // Create a new menu based on the selected conjugation type
+        switch conjugationType {
+        case Segue.indicatif:
+            btnPopTense.menu = createTenseMenu(tenses: [
+                Segue.present, Segue.passeSimple, Segue.imparfait,
+                Segue.passeCompose, Segue.futurSimple, Segue.passeAnterieur,
+                Segue.plusQueParfait, Segue.futurAnterieur
+            ])
+            btnPopTense.isEnabled = true
+
+        case Segue.participe:
+            btnPopTense.menu = createTenseMenu(tenses: [
+                Segue.present, Segue.passe
+            ])
+            btnPopTense.isEnabled = true
+
+        case Segue.infinitive:
+            btnPopTense.menu = createTenseMenu(tenses: [
+                Segue.present, Segue.passe
+            ])
+            btnPopTense.isEnabled = true
+
+        case Segue.imperatif:
+            btnPopTense.menu = createTenseMenu(tenses: [
+                Segue.present, Segue.passe
+            ])
+            btnPopTense.isEnabled = true
+
+        case Segue.subjonctif:
+            btnPopTense.menu = createTenseMenu(tenses: [
+                Segue.present, Segue.passe, Segue.imparfait, Segue.plusQueParfait
+            ])
+            btnPopTense.isEnabled = true
+
+        case Segue.conditionnel:
+            btnPopTense.menu = createTenseMenu(tenses: [
+                Segue.present, Segue.passe1reForme, Segue.passe2eForme
+            ])
+            btnPopTense.isEnabled = true
+
+        default:
+            btnPopTense.isEnabled = false
+            break
+        }
+    }
+
+    func createTenseMenu(tenses: [String]) -> UIMenu {
+        // Create a menu based on the provided tenses
+        let tenseOptions: [UIAction] = tenses.map { tense in
+            return UIAction(title: tense, state: .on, handler: tenseOptionClosure)
+        }
+
+        return UIMenu(children: tenseOptions)
+    }
+
+    func tenseOptionClosure(action: UIAction) {
+        print(action.title)
+    }
+    
     
     @IBAction func btnPopTypeValueChanged(_ sender: Any) {
         //todo test 01
+                
     }
     
     @IBAction func btnPopTenseValueChanged(_ sender: Any) {
@@ -30,15 +121,6 @@ class ThirdViewController: UIViewController {
     }
     
     
-    
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
---
+++ GoFrench/enumSegue.swift
@@ -0,0 +1,32 @@
+//
+//  enumSegue.swift
+//  GoFrench
+//
+//  Created by Lingfang He on 2023-11-24.
+//
+
+enum Segue{
+    
+    static let infinitive = "infinitive"
+    static let participe = "participe"
+    static let indicatif = "indicatif"
+    static let subjonctif = "subjonctif"
+    static let conditionnel = "conditionnel"
+    static let imperatif = "imperatif"
+    
+    
+    static let present = "present"
+    static let passe = "passe"
+    static let passeSimple = "passeSimple"
+    static let imparfait = "imparfait"
+   
+    static let passeCompose = "passeCompose"
+    static let futurSimple = "futurSimple"
+    static let passeAnterieur = "passeAnterieur"
+    static let plusQueParfait = "plusQueParfait"
+    static let futurAnterieur = "futurAnterieur"
+    
+    static let passe1reForme = "passe1reForme"
+    static let passe2eForme = "passe2eForme"
+
+}
