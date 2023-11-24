bd7af79a6ad78eedd43ee077eceac3b5921583e5
Jaspreet Kaur <ghotra.jaspreet003@gmail.com>
2023-11-24 14:10:50 -0500

    complete conjugation service implementation

--- GoFrench/SecondViewController.swift
+++ GoFrench/SecondViewController.swift
@@ -1,34 +1,44 @@
-//
-//  SecondViewController.swift
-//  GoFrench
-//
-//  Created by xi gao on 2023-11-13.
-//
-
 import UIKit
 
-class SecondViewController: UIViewController, UISearchBarDelegate {
+class SecondViewController: UIViewController {
 
-    @IBOutlet weak var searchBar: UISearchBar!
-    
     @IBOutlet weak var txtWord: UITextField!
-    
     @IBOutlet weak var txtDescription: UITextField!
-    
     @IBOutlet weak var txtWordVerbType: UITextField!
-    
-   
+    @IBOutlet weak var txtSearchTerm: UITextField!
+
+    var conjugations = [ConjugationObject]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-    
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        //todo
-    }
-    @IBAction func btnConjugate(_ sender: UIButton) {
-        //todo
+        // Additional setup after loading the view.
     }
 
+    @IBAction func btnSearchTouchUpInside(_ sender: Any) {
+        guard let searchTerm = txtSearchTerm.text, !searchTerm.isEmpty else {
+            print("Search term is empty")
+            return
+        }
+
+        let conjugationService = ConjugationService()
+        conjugationService.fetchConjugationData { [weak self] conjugationData in
+            DispatchQueue.main.async {
+                guard let data = conjugationData else {
+                    print("Error fetching conjugation data")
+                    return
+                }
+
+                // Update UI elements
+                self?.txtWord.text = data.word
+                self?.txtDescription.text = data.fullDescription
+                self?.txtWordVerbType.text = data.verbType
+
+                // Save conjugations
+                self?.conjugations = data.conjugations
+
+                // Optionally, reload a table view or other UI elements to display conjugations
+            }
+        }
+    }
 }
+
